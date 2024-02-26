efa88cbed531e0f59f292f914e289debc68a1637
Ilya Kirsanov <kirs.ilya30@gmail.com>
2024-02-26 16:05:46 +0300

    Add all files

---
+++ body_program.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int f1=0;
+    int f2=1;
+    int f3=f1+f2;
+    int f4=f3+f2;
+    int f5=f4+f3;
+    std::cout<<f1<<std::endl;
+    std::cout<<f2<<std::endl;
+    std::cout<<f3<<std::endl;
+    std::cout<<f4<<std::endl;
+    std::cout<<f5<<std::endl;
+}
